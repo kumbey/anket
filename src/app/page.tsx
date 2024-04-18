1c6dda090f9636c80a4baee54a5d5cb58fba6a10
kumbey <kumurbek.b@gmail.com>
2024-04-18 22:34:18 +0800

    admin pass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -82,9 +82,13 @@ const Home = () => {
       <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            <h2 className="mt-6  text-center text-3xl font-extrabold text-gray-900">
               Login
             </h2>
+            <h4 className="text-center text-sm  text-gray-900">
+              admin@admin.com
+            </h4>
+            <h4 className="text-center text-sm  text-gray-900">qweqwe</h4>
           </div>
           <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
             <input type="hidden" name="remember" value="true" />
